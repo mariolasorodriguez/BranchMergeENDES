1f44a698a50fb2626764981f97b2a176d0461196
Mario <mario.laso@iesnervion.es>
2026-01-19 10:36:35 +0100

    Primer commit Mario

---
+++ src/prueba/prueba.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class prueba {
+	public static void main(String[] args) {
+		System.out.println("Bienvenido al programa");
+		imprimirNumeros();
+	}
+	
+	public static void imprimirNumeros() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+	}
+}
