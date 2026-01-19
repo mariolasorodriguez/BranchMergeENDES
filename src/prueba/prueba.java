90a0478adff33d6d9c5dd82c8b5421e7bb3423b9
Mario <mario.laso@iesnervion.es>
2026-01-19 12:21:15 +0100

    Cambios en la rama mario

--- src/prueba/prueba.java
+++ src/prueba/prueba.java
@@ -4,11 +4,15 @@ public class prueba {
 	public static void main(String[] args) {
 		System.out.println("Bienvenido al programa");
 		imprimirNumeros();
+		saludo();
 	}
 	
 	public static void imprimirNumeros() {
         for (int i = 0; i < 1000; i++) {
             System.out.println(i);
         }
+	}
+	public static void saludo() {
+		System.out.println("Hablo desde la rama de Mario");
 	}
 }
